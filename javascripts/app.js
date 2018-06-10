1cdef59e85d5e07993d90432292b101ac21a2370
nallelyht <nallelyht@gmail.com>
2018-06-09 22:54:22 -0500

    first commit

---
+++ javascripts/app.js
@@ -0,0 +1,16 @@
+// Rover Object Goes Here
+
+// ======================
+
+// ======================
+function turnLeft(rover){
+  console.log("turnLeft was called!");
+}
+
+function turnRight(rover){
+  console.log("turnRight was called!");
+}
+
+function moveForward(rover){
+  console.log("moveForward was called")
+}
